88a749785fe216672dd13be6b065807e24464c8e
Joseph Ray <joseph.h.ray@gmail.com>
2017-05-11 18:21:05 +1000

    Makin stuff and doin things

---
+++ WorkforceAnalytics/DataModel/Repository/WorkerRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel.Repository
+{
+    public class WorkerRepository
+    {
+        private WorkforceContext _workforceContext;
+
+        public WorkerRepository()
+        {
+            _workforceContext = new WorkforceContext();
+        }
+
+        public Worker GetWorkerById()
+        {
+            return null;
+        }
+    }
+}
---
+++ WorkforceAnalytics/RepositoryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RepositoryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RepositoryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5eb06d5b-a86d-4e39-a2ea-f3ab20ef033a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
